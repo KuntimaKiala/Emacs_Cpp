2f94fb42cf23bb57caa590d38c592e37807e38cc
KuntimaKiala <kuntima.kiala@gmail.com>
2023-08-06 01:34:46 +0200

    FINAL

--- constant.h
+++ constant.h
@@ -1,10 +1,12 @@
-
+#include <string>
 
 namespace constants{
 
  [[maybe_unused]] constexpr double pi = 3.1415926535 ;
  [[maybe_unused]] constexpr float g =  9.18 ;
  [[maybe_unused]] constexpr int age =  31 ;
+ [[maybe_unused]] const std::string text = "K" ;
+    
     
 }
 
--- cuda.cu
+++ cuda.cu
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include "/home/kuntima/workspace/github/Emacs_Cpp/constant.h"
+#include <string>
 #define N 3
 
 
 __device__ void func(float val){
-  val += val ;
-  printf("2pi = %.9f\n",val) ;
+  val = val/2 ;
+  printf("pi/2 = %.9f\n",val) ;
 }
 
-__global__ void kernel(const float* pt, const float value){
+__global__ void kernel(const float* float_ptr, const float value){
   
-  pt = &value ;
-  func(*pt) ;
+  float_ptr = &value ;
+  func(*float_ptr) ;
 }
 
 int main(void) {
   
   
   float * pi ;
-  cudaMalloc((void **)&pi , sizeof(float)) ;
+  cudaMalloc(&pi , sizeof(float)) ;
   kernel<<<1,1>>>(pi, constants::pi);
-  
   cudaFree(pi);
   return 0 ;
 }
